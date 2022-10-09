d9109196130aac9eb20da042af1414a6a8d694dd
Andrew Marshall <andydanger12@outlook.com>
2022-10-09 18:45:13 -0500

    Adds an extra 0 to the seconds string when less than 10.

--- src/components/LiveStatusGameCard/PlayersTable.tsx
+++ src/components/LiveStatusGameCard/PlayersTable.tsx
@@ -369,8 +369,9 @@ function getInGameTime(startTime: string, currentTime: string) {
     hours = hours-(days*24);
     minutes = minutes-(days*24*60)-(hours*60);
     seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+    let secondsString = seconds < 10 ? '0' + seconds : seconds
 
-    return hours ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`
+    return hours ? `${hours}:${minutes}:${secondsString}` : `${minutes}:${secondsString}`
 }
 
 function getGoldDifference(player: ParticipantWindow, side: string, gameMetadata: GameMetadata, frame: FrameWindow) {
