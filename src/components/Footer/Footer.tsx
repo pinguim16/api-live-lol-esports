994af5bdc6a9f0f4c1ed5755837e73bb477cda56
Andrew Marshall <andydanger12@outlook.com>
2023-04-17 10:32:26 -0500

    Remove Google AdSense.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -18,15 +18,6 @@ export function Footer() {
             <a target="_blank" rel="noreferrer" href="https://discord.com/users/183408194209579008">
                 <DiscordSVG className="footer-img" />
             </a>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4163525631983528" crossOrigin="anonymous"></script>
-            <ins className="adsbygoogle"
-                data-ad-client="ca-pub-4163525631983528"
-                data-ad-slot="6000866139"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({ });
-            </script>
         </nav>
     );
 }
