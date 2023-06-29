d18b411a6bbdf3be32edefa771453905f0677156
Andrew Marshall <andydanger12@outlook.com>
2023-06-29 10:17:19 -0500

    Add LinkedIn link to Footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import './styles/footerStyle.css'
 import { ReactComponent as DiscordSVG } from '../../assets/images/discord.svg';
 import { ReactComponent as GitHubLogoSVG } from '../../assets/images/github.svg';
 import { ReactComponent as TwitterSVG } from '../../assets/images/twitter.svg';
+import { ReactComponent as LinkedInSVG } from '../../assets/images/linkedin.svg';
 
 export function Footer() {
 
@@ -17,6 +18,9 @@ export function Footer() {
             </a>
             <a target="_blank" rel="noreferrer" href="https://discord.com/users/183408194209579008">
                 <DiscordSVG className="footer-img" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/andrew-marshall-32b404110/">
+                <LinkedInSVG className="footer-img" />
             </a>
         </nav>
     );
